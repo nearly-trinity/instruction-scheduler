9f3e6785ab3564be350af1e8779a9a3212e2660b
nathan early <Nathan Early>
2022-11-28 11:57:39 -0600

    psuedocode for edges

--- allocator.cpp
+++ allocator.cpp
@@ -8,24 +8,29 @@ using namespace std;
 int debug = false;
 
 int maxVR = -1;
-struct tableRow {
+struct tableRow
+{
     int vrNumber;
     int lastUse;
 };
 std::vector<tableRow> SRtoVR;
 int vrName;
-void initTable(std::vector<tableRow> &table) {
+void initTable(std::vector<tableRow> &table)
+{
     int maxReg = getMaxReg();
-    for(int i = 0; i <= maxReg; ++i) {
-        tableRow row;        
+    for (int i = 0; i <= maxReg; ++i)
+    {
+        tableRow row;
         row.vrNumber = -1;
         row.lastUse = INT_MAX;
         SRtoVR.push_back(row);
     }
 }
 
-void update(Op &op, int idx) {
-    if(SRtoVR[op.sr].vrNumber == -1) {
+void update(Op &op, int idx)
+{
+    if (SRtoVR[op.sr].vrNumber == -1)
+    {
         SRtoVR[op.sr].vrNumber = vrName++;
     }
     op.vr = SRtoVR[op.sr].vrNumber;
@@ -34,46 +39,52 @@ void update(Op &op, int idx) {
     return;
 }
 
-void computeLastUse(std::vector<Inst> &block) {
+void computeLastUse(std::vector<Inst> &block)
+{
     vrName = 0;
     int n = block.size();
     initTable(SRtoVR);
-    for(int i = n-1; i >= 0; --i) {
+    for (int i = n - 1; i >= 0; --i)
+    {
         // cout << "at 1" << endl;
         Inst &line = block[i];
         InstructionCategory cat = line.opcode.cat;
         Instructions operation = line.opcode.op;
-        switch(cat) {
-            case MEMOP:
-                if(operation == store) {
-                    // handle store
-                    // only last two lines
-                    update(line.op1,i);
-                    update(line.op2,i);
-                } else {
-                    // handle load
-                    // first four lines
-                    update(line.dest,i);
-                    SRtoVR[line.dest.sr].vrNumber = -1;
-                    SRtoVR[line.dest.sr].lastUse = INT_MAX;
-                    update(line.op1,i);
-                }
-                break;
-            case LOADI: 
-                // first three lines
-                update(line.dest,i);
+        switch (cat)
+        {
+        case MEMOP:
+            if (operation == store)
+            {
+                // handle store
+                // only last two lines
+                update(line.op1, i);
+                update(line.op2, i);
+            }
+            else
+            {
+                // handle load
+                // first four lines
+                update(line.dest, i);
                 SRtoVR[line.dest.sr].vrNumber = -1;
                 SRtoVR[line.dest.sr].lastUse = INT_MAX;
-                break;
-            case ARITHOP:
-                update(line.dest,i);
-                SRtoVR[line.dest.sr].vrNumber = -1;
-                SRtoVR[line.dest.sr].lastUse = INT_MAX;
-                update(line.op1,i);
-                update(line.op2,i);
-                break;
-            default:
-                break;
+                update(line.op1, i);
+            }
+            break;
+        case LOADI:
+            // first three lines
+            update(line.dest, i);
+            SRtoVR[line.dest.sr].vrNumber = -1;
+            SRtoVR[line.dest.sr].lastUse = INT_MAX;
+            break;
+        case ARITHOP:
+            update(line.dest, i);
+            SRtoVR[line.dest.sr].vrNumber = -1;
+            SRtoVR[line.dest.sr].lastUse = INT_MAX;
+            update(line.op1, i);
+            update(line.op2, i);
+            break;
+        default:
+            break;
         }
     }
 }
@@ -83,35 +94,46 @@ std::vector<int> physReg;
 std::vector<int> memLocs;
 std::vector<int> nextUses;
 
-void printTable(){
+void printTable()
+{
     int padding = 4;
-    //cout << " vr |  physReg | nextUse | memLoc | "<< endl;
+    // cout << " vr |  physReg | nextUse | memLoc | "<< endl;
 
-    for(int i = 0; i < physReg.size(); i ++) {
-      if(physReg[i] != -1 || memLocs[i] != -1) {
-        cout << "//| " << i << " | " << setw(padding) << physReg[i];
-        if(nextUses[i]==INT_MAX) {
-            cout << "|"<< setw(padding) << "inf";
-        } else {
-            cout << "|"<< setw(padding) << nextUses[i];
+    for (int i = 0; i < physReg.size(); i++)
+    {
+        if (physReg[i] != -1 || memLocs[i] != -1)
+        {
+            cout << "//| " << i << " | " << setw(padding) << physReg[i];
+            if (nextUses[i] == INT_MAX)
+            {
+                cout << "|" << setw(padding) << "inf";
+            }
+            else
+            {
+                cout << "|" << setw(padding) << nextUses[i];
+            }
+            cout << " | " << setw(padding) << memLocs[i] << endl;
         }
-        cout << " | " << setw(padding) << memLocs[i] << endl;
-      }
     }
 }
 
 std::vector<Inst> resultILOC;
-int loc; int idx; int n;
+int loc;
+int idx;
+int n;
 Token blankToken = {COMMENT, {NOP, nop}, -1};
 
-int spill() {
+int spill()
+{
     int spilledVR = -999;
     int tmp_max = -999;
     // find the register with the furthest next use
-    for(int j = 0; j < vrName; ++j) {
-        // cout << "looking at vr:" << j << " nu: " << nextUses[j] << " pr: " << physReg[j] << endl; 
+    for (int j = 0; j < vrName; ++j)
+    {
+        // cout << "looking at vr:" << j << " nu: " << nextUses[j] << " pr: " << physReg[j] << endl;
         int use = nextUses[j];
-        if(use > tmp_max) {
+        if (use > tmp_max)
+        {
             // cout << "new latest use is: " << j << endl;
             tmp_max = use;
             spilledVR = j;
@@ -124,7 +146,7 @@ int spill() {
     Inst store_line(-2, {MEMOP, store}, pr, 0, -1);
     store_line.op1.pr = store_line.op1.sr;
     store_line.op2.pr = store_line.op2.sr;
-    
+
     // push spilling ILOC into result vector
     resultILOC.push_back(load_line);
     resultILOC.push_back(store_line);
@@ -136,7 +158,8 @@ int spill() {
     return pr;
 }
 
-int retrieve(int vr) {
+int retrieve(int vr)
+{
     Inst loadI_line(-1, {LOADI, loadI}, memLocs[vr], -1, 0);
     loadI_line.dest.pr = 0;
     int reg = fetchPR(vr);
@@ -150,14 +173,16 @@ int retrieve(int vr) {
     return reg;
 }
 
-int fetchPR(int vr) {
+int fetchPR(int vr)
+{
 
     // for(auto num : freeRegs) cout << num << " ";
     // cout << "\n";
     int reg;
     int spilledVR;
     // if there is an available register
-    if(freeRegs.back() != 0) {
+    if (freeRegs.back() != 0)
+    {
         // pop from the stack
         reg = freeRegs.back();
         freeRegs.pop_back();
@@ -167,94 +192,153 @@ int fetchPR(int vr) {
 
         // return the assigned pr value
         return reg;
-    } else {
+    }
+    else
+    {
         // we have to spill a value to memory
         reg = spill();
         physReg[vr] = reg;
 
         // return the register that was spilled
         return reg;
-    }   
+    }
 }
 
-int assign(int vr) {
-    if(vr != -1) {
-    // if the value is stored in memory
-        if(physReg[vr] != -1) {
+int assign(int vr)
+{
+    if (vr != -1)
+    {
+        // if the value is stored in memory
+        if (physReg[vr] != -1)
+        {
             // if the vr already has an assigned pr return it
             return physReg[vr];
-        } else if(memLocs[vr] != -1) {
+        }
+        else if (memLocs[vr] != -1)
+        {
             // fetch value from memory
             int res = retrieve(vr);
             // assign the pr to that virtual reg
             physReg[vr] = res;
             // return the newly avail PR
             return res;
-        } else {
+        }
+        else
+        {
             cout << "Something has gone very, veyr wrong" << endl;
         }
-    } else {
+    }
+    else
+    {
         return -1;
     }
 }
 
-void free(Op &op) {
-    if(op.vr != -1) {
-        if(op.nextUse == INT_MAX) {    
-            if(physReg[op.vr] == -1) cout << "THIS SHOULD NEVER HAPPEN HELP\n";
-                // if there is no next use we will free the register
+void free(Op &op)
+{
+    if (op.vr != -1)
+    {
+        if (op.nextUse == INT_MAX)
+        {
+            if (physReg[op.vr] == -1)
+                cout << "THIS SHOULD NEVER HAPPEN HELP\n";
+            // if there is no next use we will free the register
             freeRegs.push_back(physReg[op.vr]);
             physReg[op.vr] = -1;
             nextUses[op.vr] = -1;
-        } else {
+        }
+        else
+        {
             nextUses[op.vr] = op.nextUse;
         }
     }
 }
 
-std::vector<Inst> allocate(std::vector<Inst> &block, int prs) {
+std::vector<Inst> allocate(std::vector<Inst> &block, int prs)
+{
     n = block.size();
     loc = 32768;
 
     // initalize tables for storing vr data
-    physReg = std::vector<int>(vrName,-1);
-    memLocs = std::vector<int>(vrName,-1);
-    nextUses = std::vector<int>(vrName,-1);
+    physReg = std::vector<int>(vrName, -1);
+    memLocs = std::vector<int>(vrName, -1);
+    nextUses = std::vector<int>(vrName, -1);
 
     // build stack of avail physical registers
-    for(int i = 0; i < prs; ++i) {
+    for (int i = 0; i < prs; ++i)
+    {
         freeRegs.push_back(i);
     }
-    
+
     // traverse the ILOC block
     idx = 0;
-    for(auto line : block) {
-        if(debug) line.tablePrint();
-               
+    for (auto line : block)
+    {
+        if (debug)
+            line.tablePrint();
+
         // get a PR for op1.vr
         line.op1.pr = assign(line.op1.vr);
         line.op2.pr = assign(line.op2.vr);
-        
+
         free(line.op1);
         free(line.op2);
-        
-        if(line.dest.vr != -1) {
-        // get a PR for dest.vr and update table value
+
+        if (line.dest.vr != -1)
+        {
+            // get a PR for dest.vr and update table value
             fetchPR(line.dest.vr);
             line.dest.pr = physReg[line.dest.vr];
             // update next use for dest reg
             nextUses[line.dest.vr] = line.dest.nextUse;
             // free the register if not needed
-            //free(line.dest);
+            // free(line.dest);
         }
-      
+
         resultILOC.push_back(line);
-        if(debug) printTable();
-    
-        if(debug) line.tablePrint();
+        if (debug)
+            printTable();
+
+        if (debug)
+            line.tablePrint();
 
         ++idx;
     }
     return resultILOC;
 }
 
+void printNodes(std::vector<Inst> &block)
+{
+    std::cout << "standard node printing" << std::endl;
+}
+void printEdges(std::vector<Inst> &block)
+{
+    std::cout << "returning all the edges association" << std::endl;
+    /*
+    output: List[List[Int]] = [[]*number of lines]
+    lookup: List[Int] = [-1]*vrName (number of vr's)
+    for line in block:
+        line_num, label, sr1,sr2 , dest = line's registers
+        lookup[dest] = label
+
+        # depends on what type of op it is (assuming arithop)
+        if lookup[sr1] == -1 OR lookup[sr2] == -1:
+            ERROR
+
+        output[label].append(lookup[sr1])
+        output[label].append(lookup[sr2])
+
+    output: [
+        [],
+        [],
+        [2],
+        [3,4],
+        [],
+        [5,6]
+    ]
+    */
+}
+void printWeights(std::vector<Inst> &block)
+{
+    std::cout << "returning all the weighted edges (not needed for milestone)" << std::endl;
+}
--- scheduler.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "scanner.h"
-
-void printNodes(std::vector<Inst> &block) {}
-void printEdges(std::vector<Inst> &block) {}
-void printWeights(std::vector<Inst> &block) {}
